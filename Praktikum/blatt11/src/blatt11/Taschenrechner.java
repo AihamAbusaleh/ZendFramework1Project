4ea0d3f6c43783b1fc4d2617dd3dc4a6625d8a49
Aiham Abusaleh <aiabou@htwg-konstanz.de>
2015-06-25 18:07:52 +0200

    Update Taschenrechner.java

--- Praktikum/blatt11/src/blatt11/Taschenrechner.java
+++ Praktikum/blatt11/src/blatt11/Taschenrechner.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Locale;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -19,223 +20,261 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class Taschenrechner extends JFrame implements ActionListener,
-		ItemListener {
+        ItemListener {
 
-	JCheckBox helles;
+    JCheckBox helles;
 
-	JRadioButton deg;
-	JRadioButton rad;
+    JRadioButton deg;
+    JRadioButton rad;
 
-	JTextField x;
-	JTextField y;
-	JTextField z;
+    JTextField x;
+    JTextField y;
+    JTextField z;
 
-	JButton plus;
-	JButton mal;
-	JButton minus;
-	JButton div;
-	JButton sin;
-	JButton cos;
-	JButton pow;
-	JButton log2;
-	JButton clear;
+    JButton plus;
+    JButton mal;
+    JButton minus;
+    JButton div;
+    JButton sin;
+    JButton cos;
+    JButton pow;
+    JButton log2;
+    JButton clear;
 
-	public Taschenrechner() {
-		setTitle("Taschenrechner");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public Taschenrechner() {
+        setTitle("Taschenrechner");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JLabel o1 = new JLabel("Operand x");
-		JLabel o2 = new JLabel("Operand y");
-		JLabel o3 = new JLabel("Resultat");
+        JLabel o1 = new JLabel("Operand x");
+        JLabel o2 = new JLabel("Operand y");
+        JLabel o3 = new JLabel("Resultat");
 
-		x = new JTextField("0", 15);
-		y = new JTextField("0", 15);
-		z = new JTextField("0", 15);
+        x = new JTextField("0", 15);
+        y = new JTextField("0", 15);
+        z = new JTextField("0", 15);
 
-		z.setEditable(false);
+        z.setEditable(false);
 
-		plus = new JButton("+");
-		mal = new JButton("*");
-		minus = new JButton("-");
-		div = new JButton("/");
-		sin = new JButton("sin");
-		cos = new JButton("cos");
-		pow = new JButton("x^y");
-		log2 = new JButton("log2");
-		clear = new JButton("Clear");
+        plus = new JButton("+");
+        mal = new JButton("*");
+        minus = new JButton("-");
+        div = new JButton("/");
+        sin = new JButton("sin");
+        cos = new JButton("cos");
+        pow = new JButton("x^y");
+        log2 = new JButton("log2");
+        clear = new JButton("Clear");
 
-		plus.addActionListener(this);
-		mal.addActionListener(this);
-		minus.addActionListener(this);
-		div.addActionListener(this);
-		sin.addActionListener(this);
-		cos.addActionListener(this);
-		pow.addActionListener(this);
-		log2.addActionListener(this);
-		clear.addActionListener(this);
+        plus.addActionListener(this);
+        mal.addActionListener(this);
+        minus.addActionListener(this);
+        div.addActionListener(this);
+        sin.addActionListener(this);
+        cos.addActionListener(this);
+        pow.addActionListener(this);
+        log2.addActionListener(this);
+        clear.addActionListener(this);
 
-		JPanel p1 = new JPanel();
-		p1.setLayout(new GridLayout(4, 10));
-		p1.add(o1);
-		p1.add(x);
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(4, 10));
+        p1.add(o1);
+        p1.add(x);
 
-		p1.add(o2);
-		p1.add(y);
+        p1.add(o2);
+        p1.add(y);
 
-		p1.add(o3);
-		p1.add(z);
+        p1.add(o3);
+        p1.add(z);
 
-		JPanel p2 = new JPanel();
-		p2.setLayout(new GridLayout(2, 10));
-		p2.add(plus);
-		p2.add(mal);
-		p2.add(minus);
-		p2.add(div);
-		p2.add(sin);
-		p2.add(cos);
-		p2.add(pow);
-		p2.add(log2);
+        JPanel p2 = new JPanel();
+        p2.setLayout(new GridLayout(2, 10));
+        p2.add(plus);
+        p2.add(mal);
+        p2.add(minus);
+        p2.add(div);
+        p2.add(sin);
+        p2.add(cos);
+        p2.add(pow);
+        p2.add(log2);
 
-		JPanel p3 = new JPanel();
-		p3.add(clear);
+        JPanel p3 = new JPanel();
+        p3.add(clear);
 
-		// --------------------------------------------------
-		deg = new JRadioButton("Deg");
-		deg.setSelected(true);
+        // --------------------------------------------------
+        deg = new JRadioButton("Deg");
+        deg.setSelected(true);
 
-		rad = new JRadioButton("Rad");
-		rad.setSelected(false);
+        rad = new JRadioButton("Rad");
+        rad.setSelected(true);
 
-		rad.addActionListener(this);
-		deg.addActionListener(this);
+        rad.addActionListener(this);
+        deg.addActionListener(this);
 
-		ButtonGroup group = new ButtonGroup();
-		group.add(deg);
-		group.add(rad);
+        ButtonGroup group = new ButtonGroup();
+        group.add(deg);
+        group.add(rad);
 
-		JPanel raddegPanel = new JPanel();
-		raddegPanel.add(deg);
-		raddegPanel.add(rad);
+        JPanel raddegPanel = new JPanel();
+        raddegPanel.add(deg);
+        raddegPanel.add(rad);
 
-		// this.add(deg);
-		// this.add(rad);
-		// this.add(helles);
+        // this.add(deg);
+        // this.add(rad);
+        // this.add(helles);
+        // ---------------------------------------------------------------------------
+        helles = new JCheckBox("Helles Display");
+        helles.setSelected(true);
 
-		// ---------------------------------------------------------------------------
-		helles = new JCheckBox("Helles Display");
-		helles.setSelected(true);
+        helles.addItemListener(this);
+        pack();
+        setVisible(true);
+        raddegPanel.add(helles);
 
-		helles.addItemListener(this);
-		pack();
-		setVisible(true);
-		raddegPanel.add(helles);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(p1);
+        panel.add(raddegPanel);
+        panel.add(p2);
+        panel.add(p3);
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        setContentPane(panel);
+        pack();
+        setResizable(false);
+        setVisible(true);
 
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		panel.add(p1);
-		panel.add(raddegPanel);
-		panel.add(p2);
-		panel.add(p3);
-		panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-		setContentPane(panel);
-		pack();
-		setResizable(false);
-		setVisible(true);
+    }
 
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
 
-		Object source = e.getSource();
+        String s1 = x.getText();
+        String s2 = y.getText();
+        //   boolean fir = Character.isDigit(s1.charAt(0));
+        //  boolean sec = Character.isDigit(s2.charAt(0));
+        // if (!(fir && sec)) {
+        //    return;
+        // }
+        s1 = String.format(Locale.US, s1);
+        s2 = String.format(Locale.US, s2);
+        double op1;
+        double op2;
+        try {
+            op1 = Double.parseDouble(s1);
+            op2 = Double.parseDouble(s2);
+        } catch (Exception ex) {
+            return;
+        }
 
-		String s1 = x.getText();
-		String s2 = y.getText();
+        // int op1 = Integer.parseInt(s1);
+        //int op2 = Integer.parseInt(s2);
+        if (source == plus) {
+            z.setText("" + (op1 + op2));
+        }
 
-		double op1 = Double.parseDouble(s1);
-		double op2 = Double.parseDouble(s2);
+        if (source == mal) {
+            z.setText("" + (op1 * op2));
+        }
 
-		if (source == plus)
-			z.setText("" + (op1 + op2));
+        if (source == minus) {
+            z.setText("" + (op1 - op2));
+        }
 
-		if (source == mal)
-			z.setText("" + (op1 * op2));
+        if (source == div) {
+            z.setText("" + (op1 / op2));
+        }
 
-		if (source == minus)
-			z.setText("" + (op1 - op2));
+        if (source == pow) {
+            z.setText("" + Math.pow(op1, op2));
+        }
 
-		if (source == div)
-			z.setText("" + (op1 / op2));
+        if (source == log2) {
+            z.setText("" + (Math.log(op1) / Math.log(2)));
+            y.setText("0");
+        }
 
-		if (source == pow)
-			z.setText("" + Math.pow(op1, op2));
+        if (deg.isSelected()) {
+            rad.setSelected(false);
+            double degree = op1 * (Math.PI / 180);
+            double degreeSIN = Math.sin(degree);
+            if (source == sin) {
+                if (op1 == 180) {
+                    z.setText("1");
+                } else if (op1 == 360) {
+                    z.setText("0");
+                }else
+                z.setText("" + degreeSIN);
 
-		if (source == cos) {
-			z.setText("" + Math.cos(op1));
-			y.setText("0");
-		}
+            }
 
-		if (source == sin) {
-			z.setText("" + Math.sin(op1));
-			y.setText("0");
-		}
+            if (source == cos) {
+                if (op1 == 90) {
+                    z.setText("0");
+                } else {
+                    z.setText("" + Math.cos(degree));
+                }
 
-		if (source == log2) {
-			z.setText("" + (Math.log(op1) / Math.log(2)));
-			y.setText("0");
-		}
+            }
 
-		if (source == rad && source == sin)
-			z.setText("" + (Math.toRadians(Math.sin(op1))));
+        }
 
-		if (source == rad && source == cos)
-			z.setText("" + (Math.toRadians(Math.cos(op1))));
+        if (rad.isSelected()) {
+            deg.setSelected(false);
+            double degree = op1 * (Math.PI / 180);
 
-		if (source == rad && source == sin)
-			z.setText("" + (Math.toDegrees(Math.sin(op1))));
+            if (source == sin) {
+                double radianSIN = Math.sin(op1);
+                z.setText("" + radianSIN);
 
-		if (source == rad && source == cos)
-			z.setText("" + (Math.toDegrees(Math.cos(op1))));
+            }
 
-		if (source == clear) {
-			x.setText("0");
-			y.setText("0");
-			z.setText("0");
-		}
+            if (source == cos) {
+                z.setText("" + Math.cos(Math.toDegrees(degree)));
 
-	}
+            }
 
-	@Override
-	public void itemStateChanged(ItemEvent e) {
+        }
 
-		Object source = e.getItemSelectable();
+        if (source == clear) {
+            x.setText("0");
+            y.setText("0");
+            z.setText("0");
+        }
 
-		if (source == helles) {
-			x.setBackground(Color.WHITE);
-			y.setBackground(Color.WHITE);
-			z.setBackground(Color.WHITE);
+    }
 
-			x.setForeground(Color.BLACK);
-			y.setForeground(Color.BLACK);
-			z.setForeground(Color.BLACK);
+    @Override
+    public void itemStateChanged(ItemEvent e) {
 
-		}
-		if (e.getStateChange() == ItemEvent.DESELECTED) {
-			x.setBackground(Color.BLACK);
-			y.setBackground(Color.BLACK);
-			z.setBackground(Color.BLACK);
+        Object source = e.getItemSelectable();
 
-			x.setForeground(Color.YELLOW);
-			y.setForeground(Color.YELLOW);
-			z.setForeground(Color.YELLOW);
+        if (source == helles) {
+            x.setBackground(Color.WHITE);
+            y.setBackground(Color.WHITE);
+            z.setBackground(Color.WHITE);
 
-		}
+            x.setForeground(Color.BLACK);
+            y.setForeground(Color.BLACK);
+            z.setForeground(Color.BLACK);
 
-	}
+        }
+        if (e.getStateChange() == ItemEvent.DESELECTED) {
+            x.setBackground(Color.BLACK);
+            y.setBackground(Color.BLACK);
+            z.setBackground(Color.BLACK);
 
-	public static void main(String[] args) {
+            x.setForeground(Color.YELLOW);
+            y.setForeground(Color.YELLOW);
+            z.setForeground(Color.YELLOW);
 
-		JFrame myApp = new Taschenrechner();
-		myApp.setVisible(true);
-	}
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        JFrame myApp = new Taschenrechner();
+        //  myApp.setVisible(true);
+    }
 }
